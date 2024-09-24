5f55b9aff8ecbdcd4cb2387e24f0cec514f3dad2
lethyanha <lethyanha2004@gmail.com>
2024-09-25 01:18:57 +0700

    Update Account.java
    change Account

--- src/main/java/com/example/travelink/model/Account.java
+++ src/main/java/com/example/travelink/model/Account.java
@@ -2,6 +2,7 @@ package com.example.travelink.model;
 
 import java.sql.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,9 +14,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
 @Entity
-@Table(name="Account")
+@Table(name = "Account")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -25,17 +25,40 @@ public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int account_ID;
+    @Column(name = "Account_ID") // ánh xạ với tên cột trong bảng
+    private int accountId;  // Đổi thành camelCase
+
+    @Column(name = "Email", nullable = false) // cột không được null
     private String email;
+
+    @Column(name = "Password")
     private String password;
+
+    @Column(name = "CMND") // Thay đổi thành ID
     private String cmnd;
+
+    @Column(name = "Name", nullable = false) // cột không được null
     private String name;
-    private char gender;
+
+    @Column(name = "Gender") // Giới tính
+    private String gender;
+
+    @Column(name = "DateOfBirth")
     private Date dateOfBirth;
+
+    @Column(name = "PhoneNumber")
     private String phoneNumber;
+
+    @Column(name = "AvatarURL")
     private String avatarUrl;
+
+    @Column(name = "Address")
     private String address;
+
+    @Column(name = "Role", nullable = false) // cột không được null
     private int role;
+
+    @Column(name = "Status", nullable = false) // cột không được null
     private int status;
 
 }
